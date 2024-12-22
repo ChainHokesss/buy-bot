7a4f0a46ac3ebba8120502e5739a8ca6435040b6
nkurilovich <nazar.kurylovich@pynest.io>
2024-12-23 05:30:38 +0800

    fix run.py

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import logging
 import multiprocessing
 import os
 
-from aiogram.types import Update
 from aiohttp import web
 
 from src.token.handlers.token import *
@@ -20,17 +19,9 @@ async def main():
     await dp.start_polling(bot)
 
 
-async def handle_update(request):
-    json_data = await request.json()
-    update = Update(**json_data)
-    await dp.process_update(update)
-    return web.Response()
-
-
 def start_web_server():
     port = int(os.getenv("PORT", 8080))
     app = web.Application()
-    app.router.add_post('/webhook', handle_update)
     web.run_app(app, host="0.0.0.0", port=port)
 
 
